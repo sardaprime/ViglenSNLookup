56c8a32c58a31ffaab18c0c28c4fc19cf5f82617
sardaprime <sardaprime@users.noreply.github.com>
2018-07-12 18:57:17 +0100

    Add files via upload

--- ViglenSNLookup.py
+++ ViglenSNLookup.py
@@ -1,6 +1,6 @@
 import os, subprocess, urlparse, webbrowser, bs4
 
-wmicOutput = subprocess.check_output(['wmic', 'baseboard', 'get', 'serialnumber'])
-serialNumber = wmicOutput[17:]
-viglenLookup = 'http://www.viglen.co.uk/Viglen/Support/Configuration/?sn=' + serialNumber
-webbrowser.open(viglenLookup)
+wmicOutput = subprocess.check_output(['wmic', 'baseboard', 'get', 'serialnumber']) #Get serial number from WMIC
+serialNumber = wmicOutput[17:] #Slice string to just SN
+viglenLookup = 'http://www.viglen.co.uk/Viglen/Support/Configuration/?sn=' + serialNumber #Build lookup URL
+webbrowser.open(viglenLookup) #Open lookup URL
